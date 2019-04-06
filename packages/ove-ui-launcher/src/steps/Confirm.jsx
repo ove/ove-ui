f72cd36a1fb74cd0eddb913f746bdef1284b6feb
Senaka Fernando <senaka.fernando15@imperial.ac.uk>
2019-04-06 01:27:52 +0100

    Improving write-up

--- packages/ove-ui-launcher/src/steps/Confirm.jsx
+++ packages/ove-ui-launcher/src/steps/Confirm.jsx
@@ -135,9 +135,9 @@ export default class Confirm extends Component {
                                     type <code>{this.state.app}</code> in space <code>{this.state.space}</code>.<br />
                                     Please note that the application's controller may not automatically launch if you
                                     have any pop-up blockers on your web browser.<br />
-                                    The same operation can also be executed using
-                                    <a href="https://curl.haxx.se/docs/manpage.html" target="_blank" rel="noopener noreferrer"> curl</a>,
-                                    by using the command below at the commandline.
+                                    The same operation can also be executed using 
+                                    <a href="https://curl.haxx.se/docs/manpage.html" target="_blank" rel="noopener noreferrer">curl</a>
+                                    on a command line.
                                     </h3>
                             </label>
                             <div className="col-md-12">
