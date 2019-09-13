06dd8d71df5e2ed9eb25b874231b6d8b1424153e
James Scott-Brown <james@jamesscottbrown.com>
2019-09-13 20:25:04 +0100

    Disabled state inputs (#67)
    * disable either 'existing state' or 'asset url' inputs depending on selected mode
    
    * adjust indent to satisfy lint
    
    * add a newline

--- packages/ove-ui-launcher/src/steps/StateConfiguration.jsx
+++ packages/ove-ui-launcher/src/steps/StateConfiguration.jsx
@@ -178,7 +178,13 @@ export default class StateConfiguration extends Component {
                             Existing state
                         </label>
                         <div className={notValidClasses.stateCls}>
-                            <select ref="state" autoComplete="off" className="form-control" required defaultValue={this.state.state} onBlur={this.validationCheck}>
+                            <select ref="state"
+                                autoComplete="off"
+                                className="form-control"
+                                required
+                                defaultValue={this.state.state}
+                                onBlur={this.validationCheck}
+                                disabled={this.state.mode === Constants.Mode.NEW}>
                                 <option value="">Please select</option>
                                 {this._getSelectionItems()}
                             </select>
@@ -216,7 +222,8 @@ export default class StateConfiguration extends Component {
                                 className="form-control"
                                 required
                                 defaultValue={this.state.url}
-                                onBlur={this.validationCheck} />
+                                onBlur={this.validationCheck}
+                                disabled={this.state.mode === Constants.Mode.EXISTING} />
                             <div className={notValidClasses.urlValGrpCls}>{this.state.urlValMsg}</div>
                         </div>
                     </div>
