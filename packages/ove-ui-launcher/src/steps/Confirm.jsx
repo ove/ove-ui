67c4972e1a291bf3b73a3fb109341d2c0f3ed3aa
James Scott-Brown <james@jamesscottbrown.com>
2019-09-30 13:27:41 +0100

    make Confirm a stateless functional component

--- packages/ove-ui-launcher/src/steps/Confirm.jsx
+++ packages/ove-ui-launcher/src/steps/Confirm.jsx
@@ -1,40 +1,20 @@
 /* jshint ignore:start */
 // JSHint cannot deal with React.
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Constants from '../constants/launcher';
 import axios from 'axios';
-import { UnControlled as CodeMirror } from 'react-codemirror2';
+import {UnControlled as CodeMirror} from 'react-codemirror2';
 
 import 'codemirror/mode/shell/shell';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 
-import { Form, Button, Divider, Header } from 'semantic-ui-react';
+import {Form, Button, Divider, Header} from 'semantic-ui-react';
 
-export default class Confirm extends Component {
-    constructor (props) {
-        super(props);
+const Confirm = (props) => {
 
-        this.state = {
-            payload: { }
-        };
-
-        this.constructPayload = this.constructPayload.bind(this);
-        this._launch = this._launch.bind(this);
-    }
-
-    componentDidMount () {
-        this.constructPayload(this.props); // can't call from constructor, as setState can't be called on a component until it is mounted
-    }
-
-    componentDidUpdate (prevProps, prevState, snapshot) {
-        if (this.props !== prevProps) {
-            this.constructPayload(this.props);
-        }
-    }
-
-    constructPayload (props) {
+    const constructPayload = (props) => {
         let payload = {
             space: props.space,
             ...props.geometry,
@@ -62,100 +42,107 @@ export default class Confirm extends Component {
             payload.app.states.load = props.config ? parsedConfig : {};
         }
 
-        this.setState({ payload });
-    }
+        return payload;
+    };
 
-    _launch () {
+    const _launch = (payload) => {
         return new Promise((resolve, reject) => {
             const launchApp = _ => {
-                axios.post('//' + Constants.REACT_APP_OVE_HOST + '/section', this.state.payload).then(res => {
-                    if (this.props.app !== Constants.APPS.REPLICATOR.name && (res.data.id || res.data.id === 0)) {
-                        const controllerURL = '//' + Constants.REACT_APP_OVE_APP(this.props.app) +
+                axios.post('//' + Constants.REACT_APP_OVE_HOST + '/section', payload).then(res => {
+                    if (props.app !== Constants.APPS.REPLICATOR.name && (res.data.id || res.data.id === 0)) {
+                        const controllerURL = '//' + Constants.REACT_APP_OVE_APP(props.app) +
                             '/control.html?oveSectionId=' + res.data.id;
-                        this.props.updateControllerURL(controllerURL);
+                        props.updateControllerURL(controllerURL);
 
-                        if (this.props.showController) {
+                        if (props.showController) {
                             window.open(controllerURL);
                         }
                     } else {
-                        this.props.updateControllerURL(undefined);
+                        props.updateControllerURL(undefined);
                     }
                     resolve(true);
                 }).catch(error => console.log(error));
             };
-            if (this.props.deleteSections) {
-                axios.delete('//' + Constants.REACT_APP_OVE_HOST + '/sections?space=' + this.props.space).then(_ => {
+            if (props.deleteSections) {
+                axios.delete('//' + Constants.REACT_APP_OVE_HOST + '/sections?space=' + props.space).then(_ => {
                     setTimeout(launchApp, Constants.SECTION_DELETE_WAIT_TIME);
                 }).catch(error => console.log(error));
             } else {
                 launchApp();
             }
         });
-    }
+    };
 
-    _getCurlPayload () {
+    const _getCurlPayload = (payload) => {
         const DELETE_SECTIONS_COMMAND = 'curl --header "Content-Type: application/json" --request DELETE http://' +
-            Constants.REACT_APP_OVE_HOST + '/sections?space=' + this.props.space + '\n';
-        if (this.props.os === Constants.OS.UNIX) {
-            return (this.props.deleteSections ? DELETE_SECTIONS_COMMAND + '\n' : '') +
+            Constants.REACT_APP_OVE_HOST + '/sections?space=' + props.space + '\n';
+        if (props.os === Constants.OS.UNIX) {
+            return (props.deleteSections ? DELETE_SECTIONS_COMMAND + '\n' : '') +
                 'curl --header "Content-Type: application/json" --request POST --data \'' +
-                JSON.stringify(this.state.payload) + '\' http://' + Constants.REACT_APP_OVE_HOST + '/section';
+                JSON.stringify(payload) + '\' http://' + Constants.REACT_APP_OVE_HOST + '/section';
         } else {
-            return (this.props.deleteSections ? DELETE_SECTIONS_COMMAND + '\n' : '') +
+            return (props.deleteSections ? DELETE_SECTIONS_COMMAND + '\n' : '') +
                 'curl --header "Content-Type: application/json" --request POST --data ' +
-                JSON.stringify(JSON.stringify(this.state.payload)) + ' http://' + Constants.REACT_APP_OVE_HOST +
+                JSON.stringify(JSON.stringify(payload)) + ' http://' + Constants.REACT_APP_OVE_HOST +
                 '/section';
         }
-    }
+    };
 
-    render () {
-        let cmOptions = {
-            lineNumberFormatter: _ => '',
-            lineNumbers: true,
-            lineWrapping: true,
-            mode: Constants.CodeMirror.Mode.SHELL,
-            readOnly: true,
-            value: this._getCurlPayload(),
-            theme: Constants.CodeMirror.THEME
-        };
+    const payload = constructPayload(props);
 
-        const osChoices = [{ key: 'unix', value: Constants.OS.UNIX, text: 'Linux/Mac' }, { key: 'windows', value: Constants.OS.WINDOWS, text: 'Windows' }];
+    let cmOptions = {
+        lineNumberFormatter: _ => '',
+        lineNumbers: true,
+        lineWrapping: true,
+        mode: Constants.CodeMirror.Mode.SHELL,
+        readOnly: true,
+        value: _getCurlPayload(payload),
+        theme: Constants.CodeMirror.THEME
+    };
 
-        return (
-            <>
-                <Divider horizontal>
-                    <Header as='h2'>
-                        Launch Application
-                    </Header>
-                </Divider>
+    console.log(_getCurlPayload(payload))
 
-                <p>You can launch the section configured above by pressing the <code>Launch</code> button.</p>
 
-                {!this.props.showController ||
-                <p>Please note that the application&#39;s controller may not automatically launch if you
-                    have any pop-up blockers on your web browser.</p>}
+    const osChoices = [{key: 'unix', value: Constants.OS.UNIX, text: 'Linux/Mac'}, {
+        key: 'windows',
+        value: Constants.OS.WINDOWS,
+        text: 'Windows'
+    }];
 
-                <p>Alternatively, you can use a terminal to run the command listed below, which
-                    uses <a href="https://curl.haxx.se/docs/manpage.html" target="_blank"
-                    rel="noopener noreferrer">curl</a>.
-                </p>
+    return (
+        <>
+            <Divider horizontal>
+                <Header as='h2'>
+                    Launch Application
+                </Header>
+            </Divider>
 
-                <Form>
-                    <Form.Group>
-                        <Form.Select label="Operating System" value={this.props.os} options={osChoices}
-                            onChange={(_, d) => this.props.updateOS(d.value)}/>
-                    </Form.Group>
+            <p>You can launch the section configured above by pressing the <code>Launch</code> button.</p>
 
-                    <Form.Group>
-                        <CodeMirror value={this._getCurlPayload()} options={cmOptions}/>
-                    </Form.Group>
+            {!props.showController ||
+            <p>Please note that the application&#39;s controller may not automatically launch if you
+                have any pop-up blockers on your web browser.</p>}
 
-                    <Button fluid positive onClick={this._launch} disabled={!this.props.ready}>Launch!</Button>
-                </Form>
-            </>
-        );
-    }
-}
+            <p>Alternatively, you can use a terminal to run the command listed below, which
+                uses <a href="https://curl.haxx.se/docs/manpage.html" target="_blank"
+                        rel="noopener noreferrer">curl</a>.
+            </p>
+
+            <Form>
+                <Form.Group>
+                    <Form.Select label="Operating System" value={props.os} options={osChoices}
+                                 onChange={(_, d) => props.updateOS(d.value)}/>
+                </Form.Group>
+
+                <Form.Group>
+                    <CodeMirror value={_getCurlPayload(payload)} options={cmOptions}/>
+                </Form.Group>
+
+                <Button fluid positive onClick={() => _launch(payload)} disabled={!props.ready}>Launch!</Button>
+            </Form>
+        </>
+    );
+};
 
 Confirm.propTypes = {
     updateControllerURL: PropTypes.func.isRequired,
@@ -169,7 +156,9 @@ Confirm.propTypes = {
     showController: PropTypes.bool,
 
     space: PropTypes.string,
-    geometry: PropTypes.shape({ x: PropTypes.string, y: PropTypes.string, w: PropTypes.string, h: PropTypes.string }),
+    geometry: PropTypes.shape({x: PropTypes.string, y: PropTypes.string, w: PropTypes.string, h: PropTypes.string}),
     os: PropTypes.oneOf([Constants.OS.UNIX, Constants.OS.WINDOWS]),
     ready: PropTypes.bool
 };
+
+export default Confirm;
