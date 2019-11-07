ove/ove-ui

commit 5a2a0331c5455b1b053015afb2c25006be540c57

A number of obvious fixes 1. Introducing logging 2. Adding missing CSS for state where OVE is not available 3. Removing unused constants 4. Proper use of Form.Select (fixing issue in prompt not properly pointing) 5. Proper test for space selected

packages/ove-ui-launcher/src/steps/Confirm.jsx
@@ -60,12 +60,12 @@ const Confirm = (props) => {
                         props.updateControllerURL(undefined);
                     }
                     resolve(true);
-                }).catch(error => console.log(error));
+                }).catch(this.props.log.error);
             };
             if (props.deleteSections) {
                 axios.delete('//' + Constants.REACT_APP_OVE_HOST + '/sections?space=' + props.space).then(_ => {
                     setTimeout(launchApp, Constants.SECTION_DELETE_WAIT_TIME);
-                }).catch(error => console.log(error));
+                }).catch(this.props.log.error);
             } else {
                 launchApp();
             }
@@ -141,6 +141,7 @@ const Confirm = (props) => {
 };
 
 Confirm.propTypes = {
+    log: PropTypes.object.isRequired,
     updateControllerURL: PropTypes.func.isRequired,
     updateOS: PropTypes.func.isRequired,
 

packages/ove-ui-launcher/src/app/launcher.css
@@ -13,3 +13,16 @@ html {
 .ui.horizontal.divider {
     margin-top: 30px;
 }
+
+/* Required to replicate functionality of Bootstrap */
+.alert {
+    padding: 15px;
+    margin-bottom: 20px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+}
+.alert-danger {
+    color: #a94442;
+    background-color: #f2dede;
+    border-color: #ebccd1;
+}

packages/ove-ui-launcher/src/app/launcher.jsx
@@ -1,6 +1,7 @@
 /* jshint ignore:start */
 // JSHint cannot deal with React.
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SelectApp from '../steps/SelectApp';
 import SpaceAndGeometry from '../steps/SpaceAndGeometry';
 import StateConfiguration from '../steps/StateConfiguration';
@@ -16,6 +17,7 @@ export default class Launcher extends Component {
     constructor (props) {
         super(props);
 
+        this.log = props.log;
         this.updateOptions = this.updateOptions.bind(this);
         this.ready = this.ready.bind(this);
 
@@ -87,13 +89,13 @@ export default class Launcher extends Component {
                 <SelectApp updateApp={ev => this.setState({ app: ev.value, state: null, controllerURL: undefined })}
                     selectedApp={this.state.app} />
 
-                <SpaceAndGeometry updateSpace={space => this.setState({ space, controllerURL: undefined })}
+                <SpaceAndGeometry log={this.log} updateSpace={space => this.setState({ space, controllerURL: undefined })}
                     updateGeometry={geometry => this.setState({ geometry, controllerURL: undefined })}
                     updateErrors={geometryErrors => this.setState({ geometryErrors })}
 
                     space={this.state.space} geometry={this.state.geometry} errors={this.state.geometryErrors}/>
 
-                <StateConfiguration updateState={(d) => this.setState({ state: d, controllerURL: undefined })}
+                <StateConfiguration log={this.log} updateState={(d) => this.setState({ state: d, controllerURL: undefined })}
                     updateMode={d => this.setState({ mode: d, controllerURL: undefined })}
                     updateURL={d => this.setState({ url: d, controllerURL: undefined })}
                     updateErrors={d => this.setState({ stateErrors: d })}
@@ -109,7 +111,7 @@ export default class Launcher extends Component {
                     app={this.state.app} space={this.state.space} deleteSections={this.state.deleteSections} showController={this.state.showController} mode={this.state.mode} state={this.state.state} url={this.state.url} config={this.state.config}
                     errors={this.state.optionsErrors} />
 
-                <Confirm updateControllerURL={controllerURL => this.setState({ controllerURL })}
+                <Confirm log={this.log} updateControllerURL={controllerURL => this.setState({ controllerURL })}
                     updateOS={os => this.setState({ os })}
                     app={this.state.app} space={this.state.space} geometry={this.state.geometry} mode={this.state.mode} config={this.state.config} state={this.state.state} deleteSections={this.state.deleteSections} showController={this.state.showController} os={this.state.os} ready={this.ready()}/>
 
@@ -119,3 +121,7 @@ export default class Launcher extends Component {
         );
     }
 }
+
+Launcher.propTypes = {
+    log: PropTypes.object.isRequired
+};

packages/ove-ui-launcher/src/constants/launcher.jsx
@@ -33,17 +33,9 @@ const getOVEHost = function () {
 };
 
 export default {
-    UPDATE: 'UPDATE',
-    STEP: 'step',
-    PENDING: 'pending',
-    LAUNCH: 'Launch',
-    BLACKLIST: ['log', 'spaces', 'states'],
     COMPONENT_NAME: 'launcher',
     LOG_LEVEL: +(process.env.LOG_LEVEL || 5), // Level (from 0 - 6): 5 == TRACE
-    SECTION_CONTROLLER: '.section-controller',
 
-    SPACE_NAMES_LOADED_TEST_TIME: 1000, // Unit: milliseconds
-    SPACE_NAMES_LOADED_INITIAL_WAIT_TIME: 2000, // Unit: milliseconds
     SECTION_DELETE_WAIT_TIME: 1000, // Unit: milliseconds
 
     REACT_APP_OVE_HOST: getOVEHost(),
@@ -53,9 +45,6 @@ export default {
     REACT_APP_OVE_APP: function (appName) {
         return getOVEHost() + '/app/' + appName.toLowerCase();
     },
-
-    NEXT_BUTTON_CLASS: 'btn btn-prev btn-primary btn-md pull-right',
-    BACK_BUTTON_CLASS: 'btn btn-next btn-primary btn-md pull-left',
 
     DEFAULT_HEIGHT: 2424, // Unit: pixels
     DEFAULT_WIDTH: 4320, // Unit: pixels

packages/ove-ui-launcher/src/index.jsx
@@ -11,8 +11,10 @@ import './index.css';
 
 load('//' + Constants.REACT_APP_OVE_HOST + '/ove.js', _ => {
     if (window.OVE) {
-        console.log('Rendering React DOM');
-        ReactDOM.render(<Launcher />, document.getElementById('root'));
+        const log = window.OVE.Utils.Logger(Constants.COMPONENT_NAME, Constants.LOG_LEVEL);
+
+        log.debug('Rendering React DOM');
+        ReactDOM.render(<Launcher log={log} />, document.getElementById('root'));
     } else {
         console.error('[FATAL] Unable to connect to OVE Core');
         ReactDOM.render(

packages/ove-ui-launcher/src/steps/SelectApp.jsx
@@ -21,13 +21,13 @@ const SelectApp = ({ selectedApp, updateApp }) => {
             </Divider>
 
             <Form>
-                <Form.Field inline width={6}>
-                    <label>Application</label>
-                    <Form.Select
-                        value={selectedApp}
-                        error={!selectedApp && { content: 'You must select an application', pointing: 'below' }}
-                        options={appOptions} onChange={(_, d) => updateApp(d)}/>
-                </Form.Field>
+                <Form.Select
+                    label="Application"
+                    value={selectedApp}
+                    error={!selectedApp && { content: 'You must select an application', pointing: 'below' }}
+                    options={appOptions} onChange={(_, d) => updateApp(d)}
+                    width={6}
+                />
             </Form>
             <p> For details of each application, see the <a
                 href="https://ove.readthedocs.io/en/stable/ove-apps/README.html" target="_blank"

packages/ove-ui-launcher/src/steps/SpaceAndGeometry.jsx
@@ -38,9 +38,9 @@ export default class SpaceAndGeometry extends Component {
                             this.setState({ spaces: result });
                             this.determineErrors();
                         }
-                    }).catch(error => console.log(error));
+                    }).catch(this.props.log.error);
                 });
-            }).catch(error => console.log(error));
+            }).catch(this.props.log.error);
     }
 
     componentDidUpdate () {
@@ -50,7 +50,7 @@ export default class SpaceAndGeometry extends Component {
     determineErrors () {
         if (this.state.spaces.length === 0) { return; }
 
-        const spaceSelected = (this.props.space !== '');
+        const spaceSelected = (this.props.space !== undefined);
 
         const currentSpace = this.props.space;
 
@@ -137,14 +137,13 @@ export default class SpaceAndGeometry extends Component {
                 <Form>
 
                     <Form.Group>
-                        <Form.Field inline width={6}>
-                            <label>Space</label>
-                            <Form.Select options={spaceOptions}
-                                error={this.props.errors.space && { content: this.props.errors.space, pointing: 'below' }}
-                                onChange={(_, d) => this.props.updateSpace(d.value)}
-                                defaultValue={this.props.space}
-                            />
-                        </Form.Field>
+                        <Form.Select options={spaceOptions}
+                            label="Space"
+                            error={this.props.errors.space && { content: this.props.errors.space, pointing: 'above' }}
+                            onChange={(_, d) => this.props.updateSpace(d.value)}
+                            defaultValue={this.props.space}
+                            width={6}
+                        />
                         <Form.Field>
                             <label>Maximise</label>
                             <Button icon="expand" onClick={this._fillSpace}/>
@@ -203,6 +202,7 @@ export default class SpaceAndGeometry extends Component {
 }
 
 SpaceAndGeometry.propTypes = {
+    log: PropTypes.object.isRequired,
     updateSpace: PropTypes.func.isRequired,
     updateGeometry: PropTypes.func.isRequired,
     updateErrors: PropTypes.func.isRequired,

packages/ove-ui-launcher/src/steps/StateConfiguration.jsx
@@ -69,7 +69,7 @@ export default class StateConfiguration extends Component {
             .then(res => res.data)
             .then(states => {
                 this.setState({ states });
-            }).catch(error => console.log(error));
+            }).catch(this.props.log.error);
     }
 
     render () {
@@ -103,14 +103,14 @@ export default class StateConfiguration extends Component {
 
                     { ![Constants.APPS.ALIGNMENT.name, Constants.APPS.WHITEBOARD.name].includes(this.props.app) && (this.props.mode === Constants.Mode.EXISTING) &&
 
-                    <Form.Field inline width={6}>
-                        <label>Existing state</label>
-                        <Form.Select options={existingStates}
-                            value={this.props.state}
-                            onChange={(_, d) => this.props.updateState(d.value)}
-                            error={this.props.errors.existingState && { content: this.props.errors.existingState, pointing: 'above' }}
-                            required/>
-                    </Form.Field>
+                    <Form.Select options={existingStates}
+                        label="Existing state"
+                        value={this.props.state}
+                        onChange={(_, d) => this.props.updateState(d.value)}
+                        error={this.props.errors.existingState && { content: this.props.errors.existingState, pointing: 'above' }}
+                        required
+                        width={6}
+                    />
                     }
 
                     {(![Constants.APPS.ALIGNMENT.name, Constants.APPS.WHITEBOARD.name, Constants.APPS.WEBRTC.name].includes(this.props.app)) && (this.props.mode === Constants.Mode.NEW) &&
@@ -134,6 +134,7 @@ export default class StateConfiguration extends Component {
 }
 
 StateConfiguration.propTypes = {
+    log: PropTypes.object.isRequired,
     updateState: PropTypes.func.isRequired,
     updateMode: PropTypes.func.isRequired,
     updateURL: PropTypes.func.isRequired,

commit 6a57bb443ead827508fdba2bb99e0f087a8b1a50

fix error introduced in commit 5a2a0331

packages/ove-ui-launcher/src/steps/Confirm.jsx
@@ -72,12 +72,12 @@ const Confirm = (props) => {
                         props.updateControllerURL(undefined);
                     }
                     resolve(true);
-                }).catch(this.props.log.error);
+                }).catch(props.log.error);
             };
             if (props.deleteSections) {
                 axios.delete('//' + Constants.REACT_APP_OVE_HOST + '/sections?space=' + props.space).then(_ => {
                     setTimeout(launchApp, Constants.SECTION_DELETE_WAIT_TIME);
-                }).catch(this.props.log.error);
+                }).catch(props.log.error);
             } else {
                 launchApp();
             }